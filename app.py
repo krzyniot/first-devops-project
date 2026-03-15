1094e548deb6619678321865eb2031315af99cdf
Krzysiek Trojańczuk <krzyniot28@gmail.com>
2026-03-15 18:51:49 +0100

    Initial DevOps project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "1.0"
+
+@app.route("/")
+def home():
+    return f"DevOps Project - version {VERSION}"
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
